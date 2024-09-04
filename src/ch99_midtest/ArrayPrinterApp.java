47d07ca44e5dad05ec1c1395ea588152d0d17e36
pauljeoung <kuni05@naver.com>
2024-09-04 09:06:51 +0900

    feature / arrayPrinter modified

--- src/ch99_midtest/ArrayPrinterApp.java
+++ src/ch99_midtest/ArrayPrinterApp.java
@@ -6,26 +6,24 @@ public class ArrayPrinterApp {
 	public static void main (String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		String [] arrayData = null;
-		String quit = "q";
-		int userCount = scanner.nextInt();
 		
-		for (int i=0; i<userCount; i++) {
-			String arrValue = scanner.nextLine();
-			arrayData[i] = arrValue;
-			if (arrValue.equals(quit)) {
-				
-			} else {
-				continue;
-			}
+		System.out.println("배열의 길이를 입력해 주세요 > ");
+		int size = scanner.nextInt();
+		scanner.nextLine(); // 개행 처링
 		
-//		for (int k=0; i<arrayData.length; k++) {
-//			System.out.println(arrayData[k]);
-//		}
-			
+		String[] array = new String[size];
+		
+		for (int i=0; i<size; i++) {
+			System.out.println("배열["+ i +"] 안에 들어갈 내용을 입력해 주세요 > ");
+			array[i] = scanner.nextLine();
 		}
 		
+		System.out.println("배열에 입력된 값은 다음과 같습니다");
+		for (int i=0; i<array.length; i++) {
+			System.out.println("Index["+ i +"] : 입력값 > " + array[i]);
+		}
 		
+		scanner.close();
 		
 	}
 }
